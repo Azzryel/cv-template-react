b924085615b626d87e4fd92dcb4558a46a366dfe
Azzryel <AzryelShroud@gmail.com>
2021-02-03 20:36:03 +0100

    add css reset

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import GeneralInfo from "./Components/GeneralInfo";
 import EducationInfo from "./Components/EducationInfo";
 import { PracticalExp } from "./Components/PracticalExp";
+import "./styles/reset.css";
 import "./styles/App.css";
 
 class App extends Component {
